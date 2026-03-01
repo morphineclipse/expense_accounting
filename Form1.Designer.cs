46078682b50a9dadc01768399c967e0abde7f75f
morphineclipse <rg.gr.18043008@gmail.com>
2026-03-01 15:55:52 +0300

    add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,334 @@
+﻿namespace kzprs
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtAmount = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtDescription = new System.Windows.Forms.TextBox();
+            this.radioFood = new System.Windows.Forms.RadioButton();
+            this.radioTransport = new System.Windows.Forms.RadioButton();
+            this.radioFun = new System.Windows.Forms.RadioButton();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.lblFood = new System.Windows.Forms.Label();
+            this.lblTransport = new System.Windows.Forms.Label();
+            this.lblFun = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ActiveCaption;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 130);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(911, 354);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(12, 36);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(140, 20);
+            this.txtName.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Expense";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtAmount
+            // 
+            this.txtAmount.Location = new System.Drawing.Point(171, 36);
+            this.txtAmount.Name = "txtAmount";
+            this.txtAmount.Size = new System.Drawing.Size(140, 20);
+            this.txtAmount.TabIndex = 3;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(12, 86);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(140, 20);
+            this.dateTimePicker1.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(168, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(28, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Cost";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(343, 20);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Category";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(9, 70);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(30, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Date";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(168, 70);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(60, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Description";
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnDelete.Location = new System.Drawing.Point(471, 86);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(141, 20);
+            this.btnDelete.TabIndex = 11;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = false;
+            this.btnDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnAdd.Location = new System.Drawing.Point(471, 60);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(141, 20);
+            this.btnAdd.TabIndex = 12;
+            this.btnAdd.Text = "Add to list";
+            this.btnAdd.UseVisualStyleBackColor = false;
+            this.btnAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(15, 497);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(34, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Total:";
+            // 
+            // txtDescription
+            // 
+            this.txtDescription.Location = new System.Drawing.Point(171, 86);
+            this.txtDescription.Name = "txtDescription";
+            this.txtDescription.Size = new System.Drawing.Size(140, 20);
+            this.txtDescription.TabIndex = 14;
+            // 
+            // radioFood
+            // 
+            this.radioFood.AutoSize = true;
+            this.radioFood.Location = new System.Drawing.Point(346, 43);
+            this.radioFood.Name = "radioFood";
+            this.radioFood.Size = new System.Drawing.Size(49, 17);
+            this.radioFood.TabIndex = 15;
+            this.radioFood.TabStop = true;
+            this.radioFood.Text = "Food";
+            this.radioFood.UseVisualStyleBackColor = true;
+            // 
+            // radioTransport
+            // 
+            this.radioTransport.AutoSize = true;
+            this.radioTransport.Location = new System.Drawing.Point(346, 66);
+            this.radioTransport.Name = "radioTransport";
+            this.radioTransport.Size = new System.Drawing.Size(70, 17);
+            this.radioTransport.TabIndex = 16;
+            this.radioTransport.TabStop = true;
+            this.radioTransport.Text = "Transport";
+            this.radioTransport.UseVisualStyleBackColor = true;
+            // 
+            // radioFun
+            // 
+            this.radioFun.AutoSize = true;
+            this.radioFun.Location = new System.Drawing.Point(346, 89);
+            this.radioFun.Name = "radioFun";
+            this.radioFun.Size = new System.Drawing.Size(90, 17);
+            this.radioFun.TabIndex = 17;
+            this.radioFun.TabStop = true;
+            this.radioFun.Text = "Entertainment";
+            this.radioFun.UseVisualStyleBackColor = true;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(153, 497);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(34, 13);
+            this.label7.TabIndex = 18;
+            this.label7.Text = "Food:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(271, 497);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(55, 13);
+            this.label8.TabIndex = 19;
+            this.label8.Text = "Transport:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(424, 497);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(75, 13);
+            this.label9.TabIndex = 20;
+            this.label9.Text = "Entertainment:";
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Location = new System.Drawing.Point(55, 497);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(13, 13);
+            this.lblTotal.TabIndex = 21;
+            this.lblTotal.Text = "0";
+            // 
+            // lblFood
+            // 
+            this.lblFood.AutoSize = true;
+            this.lblFood.Location = new System.Drawing.Point(188, 497);
+            this.lblFood.Name = "lblFood";
+            this.lblFood.Size = new System.Drawing.Size(13, 13);
+            this.lblFood.TabIndex = 22;
+            this.lblFood.Text = "0";
+            // 
+            // lblTransport
+            // 
+            this.lblTransport.AutoSize = true;
+            this.lblTransport.Location = new System.Drawing.Point(332, 497);
+            this.lblTransport.Name = "lblTransport";
+            this.lblTransport.Size = new System.Drawing.Size(13, 13);
+            this.lblTransport.TabIndex = 23;
+            this.lblTransport.Text = "0";
+            // 
+            // lblFun
+            // 
+            this.lblFun.AutoSize = true;
+            this.lblFun.Location = new System.Drawing.Point(505, 497);
+            this.lblFun.Name = "lblFun";
+            this.lblFun.Size = new System.Drawing.Size(13, 13);
+            this.lblFun.TabIndex = 24;
+            this.lblFun.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(944, 534);
+            this.Controls.Add(this.lblFun);
+            this.Controls.Add(this.lblTransport);
+            this.Controls.Add(this.lblFood);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.radioFun);
+            this.Controls.Add(this.radioTransport);
+            this.Controls.Add(this.radioFood);
+            this.Controls.Add(this.txtDescription);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.txtAmount);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Expense tracker";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtAmount;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtDescription;
+        private System.Windows.Forms.RadioButton radioFood;
+        private System.Windows.Forms.RadioButton radioTransport;
+        private System.Windows.Forms.RadioButton radioFun;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.Label lblFood;
+        private System.Windows.Forms.Label lblTransport;
+        private System.Windows.Forms.Label lblFun;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,231 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kzprs
+{
+    public partial class Form1 : Form
+    {
+        string filePath = AppContext.BaseDirectory + "\\savedData.txt";
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            dataGridView1.RowsRemoved += (s, e) => UpdateTotals();
+
+            this.FormClosing += Form1_FormClosing;
+            this.Load += Form1_Load;
+        }
+        private void InitializeDataGridView()
+        {
+            dataGridView1.ColumnCount = 5;
+            dataGridView1.Columns[0].Name = "Date";
+            dataGridView1.Columns[1].Name = "Expense";
+            dataGridView1.Columns[2].Name = "Cost";
+            dataGridView1.Columns[3].Name = "Description";
+            dataGridView1.Columns[4].Name = "Category";
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            string expense = txtName.Text.Trim();
+            string sumText = txtAmount.Text.Trim();
+            string description = txtDescription.Text.Trim();
+            string category = "";
+            bool valid = true;
+            bool sumValid = true;
+            bool categoryChosen = true;
+
+            if (radioFood.Checked) category = "Food";
+            else if (radioTransport.Checked) category = "Transport";
+            else if (radioFun.Checked) category = "Entertainment";
+            else categoryChosen = false;
+
+            if (string.IsNullOrEmpty(expense) || string.IsNullOrEmpty(sumText) || string.IsNullOrEmpty(description))
+            {
+                valid = false;
+            }
+
+            float sum;
+            if (!float.TryParse(sumText.Replace(',', '.'), System.Globalization.NumberStyles.Float, System.Globalization.CultureInfo.InvariantCulture, out sum))
+            {
+                sumValid = false;
+            }
+
+            if (sum < 0)
+            {
+                sumValid = false;
+            }
+
+            if (!valid && !sumValid && !categoryChosen)
+            {
+                MessageBox.Show("Check the correctness of the entered data", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (!valid && (!sumValid || !categoryChosen))
+            {
+                MessageBox.Show("Check the correctness of the entered data", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (!valid)
+            {
+                MessageBox.Show("Fill in all fields", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (!sumValid)
+            {
+                MessageBox.Show("Enter a valid value", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (!categoryChosen)
+            {
+                MessageBox.Show("Select a category", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            dataGridView1.Rows.Add(dateTimePicker1.Value.ToShortDateString(), expense, sum.ToString("0.00"), description, category);
+
+            txtName.Clear();
+            txtAmount.Clear();
+            txtDescription.Clear();
+            radioFood.Checked = radioTransport.Checked = radioFun.Checked = false;
+
+            UpdateTotals();
+        }
+        private void UpdateTotals()
+        {
+            float total = 0;
+            float foodTotal = 0;
+            float transportTotal = 0;
+            float entertainmentTotal = 0;
+
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                if (row.IsNewRow) continue;
+
+                float sum;
+                if (float.TryParse(row.Cells["Cost"].Value.ToString(), out sum))
+                {
+                    string category = row.Cells["Category"].Value.ToString();
+                    total += sum;
+
+                    switch (category)
+                    {
+                        case "Food":
+                            foodTotal += sum;
+                            break;
+                        case "Transport":
+                            transportTotal += sum;
+                            break;
+                        case "Entertainment":
+                            entertainmentTotal += sum;
+                            break;
+                    }
+                }
+            }
+
+            lblTotal.Text = total > 0 ? total.ToString("0.00") : "*";
+            lblFood.Text = foodTotal > 0 ? foodTotal.ToString("0.00") : "*";
+            lblTransport.Text = transportTotal > 0 ? transportTotal.ToString("0.00") : "*";
+            lblFun.Text = entertainmentTotal > 0 ? entertainmentTotal.ToString("0.00") : "*";
+        }
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count == 0)
+            {
+                if (dataGridView1.SelectedCells.Count > 0)
+                {
+                    MessageBox.Show("Select a row to delete by clicking the empty box on the left", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+                else
+                {
+                    MessageBox.Show("Select a row to delete first", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+                return;
+            }
+
+            var confirm = MessageBox.Show("Delete the selected row?", "Confirmation", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (confirm == DialogResult.Yes)
+            {
+                foreach (DataGridViewRow row in dataGridView1.SelectedRows)
+                {
+                    if (!row.IsNewRow)
+                        dataGridView1.Rows.Remove(row);
+                }
+
+                UpdateTotals();
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (!File.Exists(filePath))
+            {
+                InitializeDataGridView();
+                return;
+            }
+
+            dataGridView1.Rows.Clear();
+            dataGridView1.Columns.Clear();
+            InitializeDataGridView();
+
+            string[] lines = File.ReadAllLines(filePath, Encoding.UTF8);
+
+            foreach (string line in lines)
+            {
+                if (string.IsNullOrWhiteSpace(line)) continue;
+
+                string[] parts = line.Split('|');
+
+                if (parts[0] == "TOTAL") lblTotal.Text = parts.Length > 1 ? parts[1] : "*";
+
+                else if (parts[0] == "FOOD") lblFood.Text = parts.Length > 1 ? parts[1] : "*";
+
+                else if (parts[0] == "TRANSPORT") lblTransport.Text = parts.Length > 1 ? parts[1] : "*";
+
+                else if (parts[0] == "FUN") lblFun.Text = parts.Length > 1 ? parts[1] : "*";
+
+                else if (parts.Length == dataGridView1.Columns.Count) dataGridView1.Rows.Add(parts);
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            List<string> lines = new List<string>();
+
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                if (row.IsNewRow) continue;
+                string[] cells = new string[row.Cells.Count];
+                for (int i = 0; i < row.Cells.Count; i++)
+                    cells[i] = row.Cells[i].Value?.ToString() ?? "";
+                lines.Add(string.Join("|", cells));
+            }
+
+            lines.Add($"TOTAL|{lblTotal.Text}");
+            lines.Add($"FOOD|{lblFood.Text}");
+            lines.Add($"TRANSPORT|{lblTransport.Text}");
+            lines.Add($"FUN|{lblFun.Text}");
+
+            File.WriteAllLines(filePath, lines, Encoding.UTF8);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
